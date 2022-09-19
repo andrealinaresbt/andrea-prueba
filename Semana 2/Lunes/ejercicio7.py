0be1d31e34d858b73a8b3454c468a2123844c2c0
Andrea Linares <linares.andrea@correo.unimet.edu.ve>
2022-09-19 17:52:58 -0400

    :bulb: Semana 2

--- Semana 2/Lunes/ejercicio7.py
+++ Semana 2/Lunes/ejercicio7.py
@@ -2,14 +2,16 @@ print('Welcome to Bella Napoli')
 option= input('Please enter an option: \n1 - vegetarian\n2 - Non vegetarian\n3 - ->')
 if option.isnumeric():
     option=int(option)
-    if option == 1:
+
+if option == 1:
        ingredient = input('please enter an ingrident: \n1- Pimiento\n2 - Tofu\n3 - ->')
        if ingredient == "1" :
         ingredient = "pimiento"
     else:
         ingredient = "tofu"
         print('the pizza is vegetarian and has {ingredient}')
-    elif option == 2:
+
+elif option == 2:
        ingredient = input('please enter an ingrident: \n1- Pepperoni\n2 - Jamon\n3 - Salmon')
       if option == 1 
        ingredient = "pepperoni"
@@ -18,8 +20,6 @@ if option.isnumeric():
      else: 
         ingredient= "salmon"
     print('the pizza is non vegetarian and has {ingredient}')
-    else:
-        print('Invalid option')
+
 else:
-    print('Error')
-is_vegetarian
+        print('Invalid option')
